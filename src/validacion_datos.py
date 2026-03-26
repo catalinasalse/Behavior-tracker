adcd76e91455703f3d140773bb773813679d860f
catalinasalse <csalse@udesa.edu.ar>
2026-03-26 15:47:21 -0300

    Add files via upload

--- src/validacion_datos.py
+++ src/validacion_datos.py
@@ -1,1 +1,33 @@
+def validar_registro(registro: dict) -> bool:
+    """
+    Verifica que un registro tenga tipos correctos y valores válidos.
 
+    Parámetros:
+    registro (dict): registro a validar.
+
+    Retorna:
+    bool: True si el registro es válido, False en caso contrario.
+    """
+
+    if type(registro["id_participante"]) != int:
+        return False
+
+    if type(registro["fecha"]) != str:
+        return False
+
+    if type(registro["app"]) != str:
+        return False
+
+    if type(registro["cantidad_uso"]) != int:
+        return False
+
+    if type(registro["tiempo_uso"]) != float:
+        return False
+
+    if registro["cantidad_uso"] < 0:
+        return False
+
+    if registro["tiempo_uso"] < 0:
+        return False
+
+    return True
